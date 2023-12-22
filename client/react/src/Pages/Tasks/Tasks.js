fe0f2eaa5d45ee715bb16630229ab0fb6410ce42
dungnq5 <>
2023-12-22 16:32:34 +0700

    Async fetching data from API

--- client/react/src/Pages/Tasks/Tasks.js
+++ client/react/src/Pages/Tasks/Tasks.js
@@ -1,5 +1,39 @@
 export default function Tasks() {
     return (<div>
-        This is tasks page
+        <div class="row">
+            <div class="col-md-4">
+                <div class="well">1</div>
+            </div>
+            <div class="col-md-4">
+                <div class="well">2</div>
+            </div>
+            <div class="col-md-4">
+                <div class="well">3</div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class="well">4</div>
+            </div>
+            <div class="col-md-4">
+                <div class="well">5</div>
+            </div>
+            <div class="col-md-4">
+                <div class="well">6</div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class="well">7</div>
+            </div>
+            <div class="col-md-4">
+                <div class="well">8</div>
+            </div>
+            <div class="col-md-4">
+                <div class="well">9</div>
+            </div>
+        </div>
     </div>);
 }
--- client/react/src/Pages/Users/Users.js
+++ client/react/src/Pages/Users/Users.js
@@ -1,5 +1,49 @@
+import { useState, useEffect } from "react";
+import { Spinner } from 'react-bootstrap';
+
 export default function Users() {
-    return (<div>
-        This is users page
-    </div>);
+    const [responseData, setResponseData] = useState({ data: [] });
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://reqres.in/api/users?page=1');
+                const jsonData = await response.json();
+                setTimeout(() => {
+                    setResponseData(jsonData);
+                    setLoading(false);
+                }, 1000);// Simulate slow http
+            } catch (error) {
+                setError(error);
+            } finally {
+
+            }
+        };
+        fetchData();
+    }, []); // The empty dependency array ensures that the effect runs only once, similar to componentDidMount
+
+    if (loading) {
+        return <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+        </Spinner>;
+    }
+    return (
+        <div class="row">
+            {responseData.data.map((user) => (
+                <div key={user.email} class="col-4">
+                    <div class="card mb-12">
+                        <div class="card-body">
+                            <h5 class="card-title">{user.first_name} {user.last_name}</h5>
+                            <div class="card-text">
+                                <img src={user.avatar} />
+                                <div style={{ textAlign: 'right' }}>{user.email}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
